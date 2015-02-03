e046497e32f30008b23c56d77900ccfaf4426958
yihan-li <yihan4.li@gmail.com>
2015-02-03 18:55:29 -0500

    Update Pandas.py

--- yihanli/Class4/Pandas.py
+++ yihanli/Class4/Pandas.py
@@ -9,6 +9,37 @@ import sqlite3
 import pandas
 conn = sqlite3.connect('/Users/YihanLi/Documents/SQLite/lahman2013.sqlite')
 
+sql = """
+select
+avg(t.AB)
+FROM
+(select
+b1.playerID, min(b1.yearID) as sec_min, b1.AB
+from
+Batting b1
+join 
+(
+SELECT b.playerID,  min(b.yearID) as rookie_year from Batting b
+GROUP BY b.playerID) b2
+on b1.playerID=b2.playerID
+join Teams t on b1.teamID=t.teamID and t.name='New York Yankees'
+where b1.yearID > b2.rookie_year
+group by 1
+) t
+where t.sec_min >=1980
+"""
+
+df=pandas.read_sql(sql, conn)
+
+#conn.close()
+
+df.fillna(0, inplace = True)
+
+#conn = sqlite3.connect('/Users/YihanLi/Documents/SQLite/lahman2013.sqlite')
+
+df.to_sql('In_class', conn, if_exists='replace', index=False)
+
+
 
 sql = """
 SELECT 
@@ -31,7 +62,7 @@ df.fillna(0, inplace = True)
 
 df.to_sql('Question1', conn, if_exists='replace', index=False)
 
-conn = sqlite3.connect('/Users/YihanLi/Documents/SQLite/lahman2013.sqlite')
+#conn = sqlite3.connect('/Users/YihanLi/Documents/SQLite/lahman2013.sqlite')
 
 sql = """
 SELECT
@@ -103,7 +134,6 @@ df.fillna(0, inplace = True)
 #conn = sqlite3.connect('/Users/YihanLi/Documents/SQLite/lahman2013.sqlite')
 
 df.to_sql('Question4', conn, if_exists='replace', index=False)
-
 
 
 
